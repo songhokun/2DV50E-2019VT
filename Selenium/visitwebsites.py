50cc7b0cf66ddce3201789879a079c55943f5b3b
Songho Lee <songho.lee@posteo.se>
2019-04-14 19:58:40 +0200

    Moved experiment setup to the appendix

--- Selenium/visitwebsites.py
+++ Selenium/visitwebsites.py
@@ -1,5 +1,5 @@
 
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -8,29 +8,6 @@ from collections import defaultdict
 import time
 import os
 import json
-
-
-def main():
-    doh_profile = __get_dns_over_https_profile()
-    driver = webdriver.Firefox(firefox_profile=doh_profile)
-
-    driver.get("http://www.python.org")
-    assert "Python" in driver.title
-    elem = driver.find_element_by_name("q")
-    elem.clear()
-    elem.send_keys("pycon")
-    elem.send_keys(Keys.RETURN)
-    time.sleep(120)
-    driver.get("https://www.naver.com")
-    time.sleep(3)
-    driver.get("https://www.clien.net")
-    time.sleep(3)
-    driver.get("https://www.index.hu")
-    time.sleep(3)
-    driver.get("http://www.python.org")
-
-    assert "No results found." not in driver.page_source
-    driver.close()
 
 
 def advanced_main():
